4b8316b96746a93003656b47f71f85360832af30
Ahmet Talha Köylü <ahmettalha.net@gmail.com>
2021-08-08 21:26:29 +0300

    Internationalization fixed for urls.py

--- bookorbooks/config/urls.py
+++ bookorbooks/config/urls.py
@@ -21,8 +21,7 @@ from django.urls.conf import include
 from rest_framework_simplejwt import views as jwt_views
 from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = i18n_patterns(
-    path('admin/', admin.site.urls),
+urlpatterns = [
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('api/book/', include('book.api.urls'), name="book"),
@@ -30,14 +29,4 @@ urlpatterns = i18n_patterns(
     path('api/account/', include('account.api.urls'), name="account"),
     path('api/school/', include('school.api.urls'), name="school"),
     path('api/quiz/', include('quiz.api.urls'), name="quiz"),
- ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-#     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-#     path('api/book/', include('book.api.urls'), name="book"),
-#     path('api/country/', include('country.api.urls'), name="country"),
-#     path('api/account/', include('account.api.urls'), name="account"),
-#     path('api/school/', include('school.api.urls'), name="school"),
-#     path('api/quiz/', include('quiz.api.urls'), name="quiz"),
-#  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + i18n_patterns(path('admin/', admin.site.urls),)
